63ee9981f0cee9aaa8fbbc6d0b06614af97f8748
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-02-14 23:24:36 +0100

    Removed unncessary use of stdafx.h

--- test/RapidJSONAdapterTest/RapidJSONAdapterTest.cpp
+++ test/RapidJSONAdapterTest/RapidJSONAdapterTest.cpp
@@ -1,8 +1,13 @@
-#include "stdafx.h"
+#include <memory>
+#include <string>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
+	::testing::FLAGS_gmock_verbose = "error";
 	::testing::InitGoogleTest(&argc, argv);
 
 	int rc = RUN_ALL_TESTS();
