22f82aac0036df6a79da4c13231581affa4dd6e7
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-03-11 00:39:45 +0100

    Pack only RapidJSONAdapter library

--- conanfile.py
+++ conanfile.py
@@ -23,7 +23,7 @@ class RapidJSONAdapterConan(ConanFile):
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.dylib*", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("libRapidJSONAdapter.a", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
