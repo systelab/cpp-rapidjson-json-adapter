89b5ee4c611934d45499cd36363c79d496c3c7bb
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-03-11 01:34:52 +0100

    Add traces

--- test_package/RapidJSONAdapterExample.cpp
+++ test_package/RapidJSONAdapterExample.cpp
@@ -9,8 +9,11 @@
 
 int main(int argc, char *argv[])
 {
+	std::cout << "Test package application started" << std::endl;
     std::unique_ptr<systelab::json::IJSONAdapter> jsonAdapter = std::make_unique<systelab::json::rapidjson::JSONAdapter>();
+	std::cout << "Adapter built" << std::endl;
     std::unique_ptr<systelab::json::IJSONDocument> document = jsonAdapter->buildDocumentFromString("{\"working\":\"false\"}");
+	std::cout << "Document built" << std::endl;
 
     systelab::json::IJSONValue& workingValue = document->getRootValue().getObjectMemberValue("working");
     workingValue.setBoolean(true);
