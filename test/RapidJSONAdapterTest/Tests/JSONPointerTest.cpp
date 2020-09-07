50ec0c784186a923c875e52da4b3ca45b1f7e431
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-09-07 20:13:45 +0200

    Added more scenarios of JSON pointers

--- test/RapidJSONAdapterTest/Tests/JSONPointerTest.cpp
+++ test/RapidJSONAdapterTest/Tests/JSONPointerTest.cpp
@@ -21,21 +21,38 @@ namespace systelab { namespace json { namespace rapidjson { namespace unit_test 
 
 		std::vector<JSONPointerTestData> testData =
 		{
+			// Valid cases
 			{ "attInt", "1234" },
 			{ "attDouble", "2.345" },
 			{ "attTrue", "true" },
 			{ "attFalse", "false" },
 			{ "attString", "\"ABC XYZ\"" },
 			{ "attArray", "[\"A\",\"B\",\"C\"]" },
+			{ "attArray/0", "\"A\"" },
+			{ "attArray/1", "\"B\"" },
+			{ "attArray/2", "\"C\"" },
 			{ "attObject", "{\"attNull\":null}" },
 			{ "attObject/attNull", "null" },
 			{ "attComplex/attObj", "{\"attSubObj\": {\"att\": 9876} }" },
 			{ "attComplex/attObj/attSubObj", "{\"att\": 9876}" },
 			{ "attComplex/attObj/attSubObj/att", "9876" },
 			{ "attComplex/attVector", "[{\"name\": \"first\"}, {\"name\": \"second\"}]" },
+			{ "attComplex/attVector/0", "{\"name\": \"first\"}" },
+			{ "attComplex/attVector/0/name", "\"first\"" },
+			{ "attComplex/attVector/1", "{\"name\": \"second\"}" },
+			{ "attComplex/attVector/1/name", "\"second\"" },
 			{ "attComplex/attNumber", "666" },
 			{ "attComplex/attString", "\"XYZ\"" },
 			{ "attComplex/attBool", "true" },
+
+			// Invalid cases
+			{ "attNotExisting", "" },
+			{ "attInt/notExisting", "" },
+			{ "attString/notExisting", "" },
+			{ "attArray/-1", "" },
+			{ "attArray/3", "" },
+			{ "attArray/noNumber", "" },
+			{ "attComplex/invalidAttribute/attSubObj", "" },
 		};
 	}
 
@@ -78,6 +95,23 @@ namespace systelab { namespace json { namespace rapidjson { namespace unit_test 
 			return serializedJSONValue;
 		}
 
+		testing::AssertionResult assertJSON(const std::string& expectedJSON, const std::string& toTestJSON)
+		{
+			if (expectedJSON.empty())
+			{
+				if (!toTestJSON.empty())
+				{
+					return testing::AssertionFailure() << "Not an empty JSON (actual=" << toTestJSON << ")";
+				}
+				
+				return testing::AssertionSuccess();
+			}
+			else
+			{
+				return compareJSONs(expectedJSON, toTestJSON, *m_jsonAdapter);
+			}
+		}
+
 	protected:
 		std::unique_ptr<JSONAdapter> m_jsonAdapter;
 		std::unique_ptr<IJSONDocument> m_jsonDocument;
@@ -88,7 +122,7 @@ namespace systelab { namespace json { namespace rapidjson { namespace unit_test 
 	{
 		auto jsonValue = m_jsonDocument->getRootValue().getJSONPointerValue(GetParam().jsonPointer);
 		std::string serializedJSONValue = serializeJSONValue(jsonValue);
-		ASSERT_TRUE(compareJSONs(GetParam().expectedSerializedJSON, serializedJSONValue, *m_jsonAdapter));
+		ASSERT_TRUE(assertJSON(GetParam().expectedSerializedJSON, serializedJSONValue));
 	}
 
 	TEST_P(JSONPointerTest, testGetJSONPointerValueConstReturnsExpectedSerializeEmptyDocument)
@@ -96,7 +130,7 @@ namespace systelab { namespace json { namespace rapidjson { namespace unit_test 
 		const auto& jsonDocumentConst = const_cast<const systelab::json::IJSONDocument&>(*m_jsonDocument);
 		const auto jsonValue = jsonDocumentConst.getRootValue().getJSONPointerValue(GetParam().jsonPointer);
 		std::string serializedJSONValue = serializeJSONValue(jsonValue);
-		ASSERT_TRUE(compareJSONs(GetParam().expectedSerializedJSON, serializedJSONValue, *m_jsonAdapter));
+		ASSERT_TRUE(assertJSON(GetParam().expectedSerializedJSON, serializedJSONValue));
 	}
 
 	INSTANTIATE_TEST_CASE_P(Test, JSONPointerTest, testing::ValuesIn(testData));
