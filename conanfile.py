72b176f74bc67570fa7c4943bf2dce38cbfa6905
fgutierrezwerfen <fgutierrez@werfen.com>
2021-07-02 17:47:03 +0200

    Updated dependencies version

--- conanfile.py
+++ conanfile.py
@@ -19,17 +19,17 @@ class RapidJSONAdapterConan(ConanFile):
         self.options["JSONAdapterTestUtilities"].gtest = self.options.gtest
 
     def requirements(self):
-        self.requires("JSONAdapterInterface/1.1.2@systelab/stable")
+        self.requires("JSONAdapterInterface/1.1.3@systelab/stable")
         self.requires("rapidjson/1.1.0.1@systelab/stable")
 
     def build_requirements(self):
-        self.build_requires("JSONAdapterTestUtilities/1.1.2@systelab/stable")
+        self.build_requires("JSONAdapterTestUtilities/1.1.3@systelab/stable")
         if self.options.gtest == "1.7.0":
             self.build_requires("gtest/1.7.0@systelab/stable")
         elif self.options.gtest == "1.8.1":
             self.build_requires("gtest/1.8.1")
         else:
-            self.build_requires("gtest/1.10.0#0c895f60b461f8fee0da53a84d659131")
+            self.build_requires("gtest/1.10.0")
 
     def build(self):
         cmake = CMake(self)
